1384ee797a3dc7760b08b7cd436e16199da1b602
Slappa <jgeorgedevelopment@gmail.com>
2026-01-16 00:28:31 -0500

    Task solution

--- src/test/java/codefinity/StringProcessorTest.java
+++ src/test/java/codefinity/StringProcessorTest.java
@@ -10,38 +10,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringProcessorTest {
 
-    //ToDo: Remove the list creation for all the tests, replace it with list initialization
+    private List<String> input;
+    private List<String> expected;
+    private List<String> actual;
 
+    //ToDo: Remove the list creation for all the tests, replace it with list initialization
     @BeforeEach
     public void setUp() {
-        List <String> input;
-        List <String> expected;
-        List <String> actual;
+        List<String> input;
+        List<String> expected;
+        List<String> actual;
     }
 
     @Test
     public void testFilterAndSortPalindromes() {
-        List<String> input = Arrays.asList("radar", "hello", "level", "world", "madam", "java");
-        List<String> expected = Arrays.asList("radar", "level", "madam");
-        List<String> actual = StringProcessor.filterAndSortPalindromes(input);
+        input = Arrays.asList("radar", "hello", "level", "world", "madam", "java");
+        expected = Arrays.asList("radar", "level", "madam");
+        actual = StringProcessor.filterAndSortPalindromes(input);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testFilterAndSortPalindromesWithEmptyList() {
-        List<String> input = Arrays.asList();
-        List<String> expected = Arrays.asList();
-        List<String> actual = StringProcessor.filterAndSortPalindromes(input);
+        input = Arrays.asList();
+        expected = Arrays.asList();
+        actual = StringProcessor.filterAndSortPalindromes(input);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testFilterAndSortPalindromesWithNoPalindromes() {
-        List<String> input = Arrays.asList("java", "hello", "world");
-        List<String> expected = Arrays.asList();
-        List<String> actual = StringProcessor.filterAndSortPalindromes(input);
+        input = Arrays.asList("java", "hello", "world");
+        expected = Arrays.asList();
+        actual = StringProcessor.filterAndSortPalindromes(input);
         assertEquals(expected, actual);
     }
 
     //ToDo: Write a method that will test if the sorting in the method works correctly
+    @Test
+    public void testFilterAndSortPalindromesWithMultiplePalindromes(){
+        input = Arrays.asList("world", "banana", "racecar", "bob", "Never odd or even");
+        expected = Arrays.asList( "Never odd or even", "racecar", "bob");
+        actual = StringProcessor.filterAndSortPalindromes(input);
+        assertEquals(expected, actual);
+    }
 }
